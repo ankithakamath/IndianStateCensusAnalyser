919b2e8235e3a2fd39dd85cb8fd8b569fb7b1866
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-30 08:58:30 +0530

    The Uc1 for census is added

---
+++ src/main/java/indianstatecensusAnalyser/StateCensusAnalyser.java
@@ -0,0 +1,61 @@
+package indianstatecensusAnalyser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	private String CSV_FILE_PATH = "";
+
+	public StateCensusAnalyser() {
+
+	}
+
+	public StateCensusAnalyser(String SAMPLE_CSV_FILE_PATH) {
+		this.CSV_FILE_PATH = SAMPLE_CSV_FILE_PATH;
+	}
+
+	/**
+	 * @return the number of data read from the csv file
+	 * 
+	 */
+	public int readStateRecord() throws CsvException {
+		int count = 0;
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+			@SuppressWarnings("unchecked")
+			CsvToBean<CsvStateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(CsvStateCensus.class)
+					.withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<CsvStateCensus> csvUserIterator = csvToBean.iterator();
+			while (csvUserIterator.hasNext()) {
+				CsvStateCensus state = csvUserIterator.next();
+				count++;
+
+				if (state.getState() == null || state.getPopulation() == 0 || state.getAreaInSqKm() == 0) {
+					throw new CsvException(CsvException.ExceptionType.INCORRECT_HEADER, "Header doesn't match");
+				}
+			}
+		}
+
+		catch (NoSuchFileException e) {
+			if (CSV_FILE_PATH.contains(".csv")) {
+				throw new CsvException(CsvException.ExceptionType.FILE_NOT_FOUND, "File not found");
+			}
+			throw new CsvException(CsvException.ExceptionType.INCORRECT_TYPE, "Wrong Type");
+
+		} catch (RuntimeException e) {
+
+			throw new CsvException(CsvException.ExceptionType.DELEMETER_NOT_FOUND, "Wrong Delimiter");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+		return count;
+	}
+}
